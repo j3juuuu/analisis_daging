1ef32928a61356eab792d8b1135699d23e917691
j3juuuu <najwafikriyatull@gmail.com>
2026-06-21 14:01:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request
-import os
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = "static/uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -15,17 +11,13 @@ def index():
         file = request.files.get("image")
 
         if not file or file.filename == "":
-            error = "Pilih gambar dulu"
+            error = "Silakan pilih gambar terlebih dahulu."
         else:
-            path = os.path.join(UPLOAD_FOLDER, file.filename)
-            file.save(path)
-
             result = {
                 "filename": file.filename,
-                "status": "Upload berhasil di Vercel"
+                "status": "Upload berhasil"
             }
 
     return render_template("index.html", result=result, error=error)
 
-
-app = app
+application = app
