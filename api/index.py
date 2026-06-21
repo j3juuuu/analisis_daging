542b0f55e239b10e033a6c3f30b63664c2427c2c
j3juuuu <najwafikriyatull@gmail.com>
2026-06-21 16:39:06 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,7 +3,10 @@ import json
 def handler(request):
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
         "body": json.dumps({
-            "status": "API jalan"
+            "status": "API berhasil jalan di Vercel"
         })
     }
